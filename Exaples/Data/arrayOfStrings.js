03d5bd9ddbcefcd7bcfcb6100deea04bae42ad59
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-07-20 02:10:20 +0300

    Data example

---
+++ Exaples/Data/arrayOfStrings.js
@@ -0,0 +1,14 @@
+[
+  'Marcus Aurelius Antoninus Augustus',
+  'Darth Vader',
+  'Victor Michailovich Glushkov',
+  'Gottfried Wilhelm von Leibniz',
+  'Mao Zedong',
+  'Vladimir Sergeevich Soloviov',
+  'Ibn Arabi',
+  'Lev Nikolayevich Tolstoy',
+  'Muammar Muhammad Abu Minyar al-Gaddafi',
+  'Rene Descartes',
+  'Fyodor Mikhailovich Dostoyevsky',
+  'Benedito de Espinosa'
+]
