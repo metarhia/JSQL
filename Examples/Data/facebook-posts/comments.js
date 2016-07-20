a7b30694bfaef157fbb7572a127bd2f20e607330
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-07-20 13:32:49 +0300

    Fix gramatical mistakes. Added examples of facebook posts and Star Wars universe

---
+++ Examples/Data/facebook-posts/comments.js
@@ -0,0 +1,28 @@
+{
+  CommentList: [
+    {
+      _id: 'cid-38292594',
+      content: 'This is a very good blog post',
+      author: 'pahan'
+    },
+  
+    {
+      _id: 'cid-32235286',
+      content: 'Keep up the good work',
+      author: 'indi'
+    },
+  ],
+  ReplyList: [
+    {
+      _id: 'cid-33986932',
+      content: 'Thank You!',
+      author: 'arunoda'
+    },
+  
+    {
+      _id: 'cid-85273772',
+      content: 'If you need more information, just contact me.',
+      author: 'arunoda'
+    },
+  ],
+}
